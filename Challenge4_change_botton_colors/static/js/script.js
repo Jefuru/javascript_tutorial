e18ab06659a9d670a8099dea91f5bfc6ca09c113
jefuru <lcf4020708@gmail.com>
2020-05-27 12:03:47 +0800

    Challenge4_change_button_colors

---
+++ Challenge4_change_botton_colors/static/js/script.js
@@ -0,0 +1,50 @@
+let all_buttons = document.getElementsByTagName('button');
+
+let copyAllButtons = [];
+for(let i = 0; i < all_buttons.length; i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+console.log(copyAllButtons)
+
+function buttonColorChange(buttonThingy){
+    if(buttonThingy.value === 'red'){
+        buttonsRed();
+    } else if(buttonThingy.value === 'green'){
+        buttonsGreen();
+    } else if(buttonThingy.value === 'reset'){
+        buttonColorReset();
+    } else if(buttonThingy.value === 'random'){
+        randomColors();
+    }
+}
+
+function buttonsRed(){
+    for (let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonsGreen(){
+    for (let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset(){
+    for(let i = 0; i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors(){
+    let choices = ['btn-primary', 'btn-danger', 'btn-warning', 'btn-success']
+    for(let i = 0; i < all_buttons.length; i++){
+        let randomNumber = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
